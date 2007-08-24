8e8e4e04e30bfc0c3f890600539d7d2149701891
Alexey Gladkov <legion@altlinux.org>
2007-08-24 18:37:35 +0400

    Almost all was rewritten, but with a same idea
    Signed-off-by: Alexey Gladkov <legion@altlinux.org>

--- config.mk
+++ config.mk
@@ -1,26 +1,26 @@
 ### Global variables
-TEMPDIR = /tmp
-APTBOXDIR = $(TEMPDIR)/aptbox
-TOOLSDIR = /usr/share/mkimage/tools
-CONFIGDIR = /usr/share/mkimage
+ifndef MKI_PREFIX
+MKI_PREFIX = /usr/share/mkimage
+endif
+
+CONFIGDIR = $(MKI_PREFIX)
+TOOLSDIR = $(MKI_PREFIX)/tools
 
 ### Global helpers
-MKAPTBOX		= $(TOOLSDIR)/mki-mkaptbox
-RESULT_CHANGE		= $(TOOLSDIR)/mki-result-check
-CHROOT_SCRIPTS		= $(TOOLSDIR)/chroot-scripts
+CHROOT_MKAPTBOX		= $(TOOLSDIR)/mki-mkaptbox
+CHROOT_CACHE		= $(TOOLSDIR)/mki-cache
 
-# workdir tools
-CHROOT_PREPARE 		= $(TOOLSDIR)/chroot-prepare
-CHROOT_PACK 		= $(TOOLSDIR)/chroot-pack
-CHROOT_CLEAN 		= $(TOOLSDIR)/chroot-clean
-CHROOT_EXEC 		= $(TOOLSDIR)/chroot-exec
-CHROOT_RUN 		= $(TOOLSDIR)/chroot-run
-
-# subworkdir tools
-CHROOT_COPY_PKGS	= $(TOOLSDIR)/chroot-copy-pkgs
-CHROOT_COPY_TREE	= $(TOOLSDIR)/chroot-copy-tree
-CHROOT_INSTALL		= $(TOOLSDIR)/chroot-install
-CHROOT_IMAGE_INSTALL	= $(TOOLSDIR)/chroot-image-install
+CHROOT_SCRIPTS		= $(TOOLSDIR)/mki-scripts
+CHROOT_PREPARE 		= $(TOOLSDIR)/mki-prepare
+CHROOT_PACK 		= $(TOOLSDIR)/mki-pack
+CHROOT_CLEAN 		= $(TOOLSDIR)/mki-clean
+CHROOT_EXEC 		= $(TOOLSDIR)/mki-exec
+CHROOT_RUN 		= $(TOOLSDIR)/mki-run
+CHROOT_COPY_SUBDIRS	= $(TOOLSDIR)/mki-copy-subdirs
+CHROOT_COPY_PKGS	= $(TOOLSDIR)/mki-copy-pkgs
+CHROOT_COPY_TREE	= $(TOOLSDIR)/mki-copy-tree
+CHROOT_INSTALL		= $(TOOLSDIR)/mki-install
+CHROOT_IMAGE_INSTALL	= $(TOOLSDIR)/mki-image-install
 
 ### Config global variables
 TARGET = i586
@@ -30,20 +30,78 @@ NO_CACHE =
 
 HSH_APT_CONFIG = 
 HSH_APT_PREFIX = 
+HSH_CACHEDIR = $(CACHEDIR)/hsh
 HSH_NUMBER = 
 
 ### Per-image variables
 SUBDIRS = 
 
+MKI_SCRIPTDIR = $(CURDIR)/scripts.d
+
+# Instrumental chroot packages list or filename with packages list.
 MKI_INITROOT_REQUIRES =
+
+# Work chroot packages list.
 MKI_REQUIRES =
 
-MKI_DATA_TREE =
 MKI_DESTDIR =
-MKI_DATAIMAGE =
 
-### Pack image
+# Specifies a directory to copy in to $(SUBWORKDIR) as is.
+MKI_DATA_TREE =
+
+# Do not build sub chroot.
+MKI_NO_SUBCHROOT =
+
+### Pack image 
+# Defines a way of results packing ($(SUBWORKDIR) as root).
+#
+# squash  - create a squashfs filesystem.
+# tarbz2  - create a tar compessed by bzip2.
+# isoboot - create a bootable ISO image.
+# isodata - create a data ISO image.
+# data    - copy $(SUBWORKDIR) as is.
+#
+MKI_PACKTYPE =
+
+# Output image name (useful for squash, tarbz2, isoboot and isodata types).
 MKI_OUTNAME =
 
-# MKI_PACKTYPE := squash | tar.bz2 | tar.gz
-MKI_PACKTYPE =
+###
+### isoboot variables
+###
+
+# Modules list for propagator.
+ISOBOOT_MAR_MODULES =
+
+# Specifies a propagator initfs file. It is required for creation of file system.
+ISOBOOT_INITFS =
+
+# Specifies a propagator top message.
+ISOBOOT_VERSION =
+
+# Specifies a text string that will be written into the volume header. See: man mkisofs(8).
+ISOBOOT_APPI = 
+
+# Specifies the Copyright file name. See: man mkisofs(8).
+ISOBOOT_COPY =
+
+# Specifies the abstract file name. See: man mkisofs(8).
+ISOBOOT_ABST =
+
+# Specifies the bibliographic file name. See: man mkisofs(8).
+ISOBOOT_BIBL =
+
+# Specifies a text string that will be written into the volume header. See: man mkisofs(8).
+ISOBOOT_PREP =
+
+# Specifies  a  text  string that will be written into the volume header. See: man mkisofs(8).
+ISOBOOT_PUBL =
+
+# Specifies  the  system  ID. See: man mkisofs(8).
+ISOBOOT_SYSI =
+
+# Specifies  the  volume  ID  (volume name or label) to be written into the master block. See: man mkisofs(8).
+ISOBOOT_VOLI =
+
+# Specifies the volset ID. See: man mkisofs(8).
+ISOBOOT_VOLS =
--- rules.mk
+++ rules.mk
@@ -1,59 +1,65 @@
-WORKDIR = $(CURDIR)/.workdir
-OUTDIR = $(WORKDIR)/.outdir
-CACHEDIR = $(WORKDIR)/.cache
-MYMAKEFILE = $(CURDIR)/Makefile
+WORKDIRNAME	= .work
+OUTDIRNAME	= .out
+CACHEDIRNAME	= .cache
+
+MYMAKEFILE 	= $(CURDIR)/Makefile
+WORKDIR 	= $(CURDIR)/$(WORKDIRNAME)
+OUTDIR		= $(WORKDIR)/$(OUTDIRNAME)
+CACHEDIR	= $(WORKDIR)/$(CACHEDIRNAME)
+PKGBOX		= $(WORKDIR)/pkgbox
 
 .PHONY: $(SUBDIRS)
 .EXPORT_ALL_VARIABLES:
 
 all: $(SUBDIRS)
 
-##################################################
-### init
-##################################################
 prepare: $(SUBDIRS)
-	mkdir -p -- $(APTBOXDIR) $(WORKDIR) $(OUTDIR) $(CACHEDIR)
-	$(MKAPTBOX)
+	mkdir -p -- $(PKGBOX) $(WORKDIR) $(OUTDIR)
+	mkdir -p -- $(CACHEDIR)/mki $(CACHEDIR)/hsh
 
 prepare-workdir: prepare $(SUBDIRS)
-	$(CHROOT_PREPARE)
+	@echo $(PATH)
+	if ! $(CHROOT_CACHE) check prepare-workdir; then \
+	    $(CHROOT_PREPARE) || exit 1; \
+	    $(CHROOT_CACHE) build prepare-workdir || exit 1; \
+	fi
 
-##################################################
-### hooks
-##################################################
 run-scripts: prepare-workdir $(SUBDIRS)
-	$(CHROOT_SCRIPTS)
+	$(CHROOT_SCRIPTS) || exit 1
 
-##################################################
-### copy data
-##################################################
 copy-tree: prepare-workdir $(SUBDIRS)
-	$(CHROOT_COPY_TREE)
+	if ! $(CHROOT_CACHE) check copy-tree; then \
+	    $(CHROOT_COPY_TREE) || exit 1; \
+	    $(CHROOT_CACHE) build copy-tree || exit 1; \
+	fi
 
-##################################################
-### build image
-##################################################
 build-data: prepare-workdir $(SUBDIRS)
-	$(CHROOT_COPY_PKGS)
+	if ! $(CHROOT_CACHE) check build-data; then \
+	    $(CHROOT_COPY_PKGS) || exit 1; \
+	    $(CHROOT_CACHE) build build-data || exit 1; \
+	fi
 
 build-image: prepare-workdir $(SUBDIRS)
-	$(CHROOT_IMAGE_INSTALL) $(MKI_REQUIRES)
+	if ! $(CHROOT_CACHE) check build-image; then \
+	    $(CHROOT_IMAGE_INSTALL) $(MKI_REQUIRES) || exit 1; \
+	    $(CHROOT_CACHE) build build-image || exit 1; \
+	fi
 
-##################################################
-### pack
-##################################################
 pack-image: prepare-workdir $(SUBDIRS)
-	$(CHROOT_PACK)
+	if [ -n "$(SUBDIRS)" ] && ! $(CHROOT_CACHE) check subdirs; then \
+	    $(CHROOT_COPY_SUBDIRS) || exit 1; \
+	    $(CHROOT_CACHE) build subdirs || exit 1; \
+	fi
+	if ! $(CHROOT_CACHE) check pack-image; then \
+	    $(CHROOT_PACK) || exit 1; \
+	    $(CHROOT_CACHE) build pack-image || exit 1; \
+	fi
 
-##################################################
-### cleanup
-##################################################
 clean: $(SUBDIRS)
 	[ ! -d "$(WORKDIR)" ] || $(CHROOT_CLEAN)
 
 distclean: clean $(SUBDIRS)
-	rm -rf -- $(WORKDIR) $(OUTDIR) $(CACHEDIR) $(APTBOXDIR)/aptbox
+	rm -rf -- $(WORKDIR) $(OUTDIR) $(CACHEDIR) $(PKGBOX)
 
-##################################################
 $(SUBDIRS):
 	$(MAKE) $(MFLAGS) -C $@ $(MAKECMDGOALS)
