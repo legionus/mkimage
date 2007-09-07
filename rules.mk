272d2f6d5ea1ed7661d6cdd8c8becd55310f5172
Alexey Gladkov <legion@altlinux.org>
2007-09-07 14:15:57 +0400

    Optimize rules
    Signed-off-by: Alexey Gladkov <legion@altlinux.org>

--- rules.mk
+++ rules.mk
@@ -19,13 +19,13 @@ prepare: $(SUBDIRS)
 
 prepare-workdir: prepare $(SUBDIRS)
 	if ! $(CHROOT_CACHE) check prepare-workdir; then \
-	    $(CHROOT_PREPARE) || exit 1; \
+	    $(CHROOT_PREPARE) && \
 	    $(CHROOT_CACHE) build prepare-workdir || exit 1; \
 	fi
 
 prepare-image-workdir: prepare-workdir $(SUBDIRS)
 	if ! $(CHROOT_CACHE) check prepare-image-workdir; then \
-	    $(CHROOT_IMAGE_PREPARE) || exit 1; \
+	    $(CHROOT_IMAGE_PREPARE) && \
 	    $(CHROOT_CACHE) build prepare-image-workdir || exit 1; \
 	fi
 
@@ -40,31 +40,31 @@ run-scripts: prepare-workdir $(SUBDIRS)
 
 copy-tree: prepare-workdir $(SUBDIRS)
 	if ! $(CHROOT_CACHE) check copy-tree; then \
-	    $(CHROOT_COPY_TREE) || exit 1; \
+	    $(CHROOT_COPY_TREE) && \
 	    $(CHROOT_CACHE) build copy-tree || exit 1; \
 	fi
 
 copy-subdirs: prepare-workdir $(SUBDIRS)
 	if [ -n "$(SUBDIRS)" ] && ! $(CHROOT_CACHE) check subdirs; then \
-	    $(CHROOT_COPY_SUBDIRS) || exit 1; \
+	    $(CHROOT_COPY_SUBDIRS) && \
 	    $(CHROOT_CACHE) build subdirs || exit 1; \
 	fi
 
 copy-packages: prepare-workdir $(SUBDIRS)
 	if ! $(CHROOT_CACHE) check copy-packages; then \
-	    $(CHROOT_COPY_PKGS) || exit 1; \
+	    $(CHROOT_COPY_PKGS) && \
 	    $(CHROOT_CACHE) build copy-packages || exit 1; \
 	fi
 
 build-image: prepare-image-workdir $(SUBDIRS)
 	if ! $(CHROOT_CACHE) check build-image; then \
-	    $(CHROOT_IMAGE_INSTALL) $(MKI_REQUIRES) || exit 1; \
+	    $(CHROOT_IMAGE_INSTALL) $(MKI_REQUIRES) && \
 	    $(CHROOT_CACHE) build build-image || exit 1; \
 	fi
 
 pack-image: prepare-workdir copy-subdirs $(SUBDIRS)
 	if ! $(CHROOT_CACHE) check pack-image; then \
-	    $(CHROOT_PACK) || exit 1; \
+	    $(CHROOT_PACK) && \
 	    $(CHROOT_CACHE) build pack-image || exit 1; \
 	fi
 
